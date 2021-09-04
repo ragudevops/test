72406d0dc063c0b856246354bb8eff502c15e7ba
ragudevops <90095743+ragudevops@users.noreply.github.com>
2021-09-04 19:24:14 +0530

    code

---
+++ solution/gencsv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 0, 234 >> inputfile
+echo 1, 98 >> inputfile
+echo 2, 34 >> inputfile
+echo 3, 45 >> inputfile
+echo 4, 78 >>inputfile
+echo 5, 50 >>inputfile
+echo 6, 22 >> inputfile
+echo 7, 11 >> inputfile
+echo 8, 66 >> inputfile
+echo 9, 33 >> inputfile
+echo 10, 75 >> inputfile
